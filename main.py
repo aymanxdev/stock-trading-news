db059ccd57f06467dbcbcae2fdab0948cc79c4a7
Ayman A <61881131+aymanxdev@users.noreply.github.com>
2021-08-25 22:59:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -56,13 +56,4 @@ def compare_price(current, previous):
 
 print(compare_price(yesterday_closing_price, before_yesterday_price))
 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
 
